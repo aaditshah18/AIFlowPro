8c6904bdeadb253d12c261a767f5682422b62e7d
Hey.Its_RJ <rishab1300@gmail.com>
2024-06-24 21:57:08 -0400

    added download from GC bucket

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,14 @@
-from fastapi import FastAPI, Query
+from fastapi import FastAPI, Query, BackgroundTasks
 from fastapi.middleware.cors import CORSMiddleware
 from routers import flight_router
 from fastapi.responses import RedirectResponse
 from utils.gcp_model import download_blob
 import uvicorn
 from dotenv import load_dotenv
+from utils.subscribe_bucket_noti import create_subscriber
+import asyncio
+import threading
+import os
 
 load_dotenv()
 
@@ -18,12 +22,24 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+def start_subscriber_loop():
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    loop.run_until_complete(create_subscriber())
 
 @app.on_event("startup")
 async def startup():
-    download_blob('final-lab-model-bucket', 'models/model.pkl', 'assets/model.pkl')
+    download_blob('final-lab-model-bucket', 'models/model.pkl', 'backend/assets/model.pkl')
+    download_blob('final-lab-model-bucket', 'models/preprocessorlr.pkl', 'backend/assets/preprocessor.pkl')
+    thread = threading.Thread(target=start_subscriber_loop)
+    thread.start()
     app.include_router(flight_router.router)
 
+@app.on_event("shutdown")
+async def shutdown_event():
+    print("Application is shutting down...")
+    os.remove('backend/assets/model.pkl')
+    os.remove('backend/assets/preprocessor.pkl')
 
 @app.get("/", include_in_schema=False)
 def docs_redirect():
